a1b29445284cc1eeef64073756dc673af9f240e4
bruna <bruhpqt@gmail.com>
2025-08-27 22:12:39 -0300

    etapa 2

--- moviedb/__init__.py
+++ moviedb/__init__.py
@@ -1,26 +1,47 @@
 import json
+import logging
 import os
+import sys
 
 from flask import Flask
 
+from moviedb.infra import app_logging
+from moviedb.infra.modulos import bootstrap
 
-def create_app(config_filename: str = 'config.dev.jason')->Flask:
+
+def create_app(config_filename: str = 'config.dev.json') -> Flask:
     app = Flask(__name__,
-                    instance_relative_config=True,
-                    static_folder = 'static',
-                    template_folder = 'templates'
-                    )
+                instance_relative_config=True,
+                static_folder='static',
+                template_folder='templates',
+                )
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    try:
-        app.config.from_file(config_filename, load=json.load)
-        app.logger.debug('Config loaded')
-    except FileNotFoundError:
-        app.logger.error("Arquivo nao encontrado")
-        exit(1)
+    app_logging.configure_logging(logging.DEBUG)
 
+    app.logger.debug("Configurando a aplicação a partir do arquivo '%s'" % (config_filename))
+    try:
+        app.config.from_file(config_filename,
+                             load=json.load)
+    except FileNotFoundError:
+        app.logger.fatal("O arquivo de configuração '%s' não existe" % (config_filename))
+        sys.exit(1)
+
+    app.logger.debug("Registrando modulos")
+    bootstrap.init_app(app)
+
+    app.logger.debug("Registrando blueprints")
+    from moviedb.blueprints.root import bp as root_bp
+    app.register_blueprint(root_bp)
+
+    app.logger.debug("Definindo processadores de contexto")
+
+    @app.context_processor
+    def inject_globals():
+        return dict(app_config=app.config)
 
     return app
